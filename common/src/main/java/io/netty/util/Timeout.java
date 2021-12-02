f51002d6f4e2288e1f1e3ef17783b546bf1ac86c
haifeng9414 <dong_hf@yeah.net>
2021-12-02 16:21:23 +0800

    添加笔记

--- common/src/main/java/io/netty/util/Timeout.java
+++ common/src/main/java/io/netty/util/Timeout.java
@@ -19,28 +19,33 @@ package io.netty.util;
  * A handle associated with a {@link TimerTask} that is returned by a
  * {@link Timer}.
  */
+// 表示HashedWheelTimer时间轮中的一个任务对象，即任务对象的句柄
 public interface Timeout {
 
     /**
      * Returns the {@link Timer} that created this handle.
      */
+    // 返回当前任务所在的时间轮对象
     Timer timer();
 
     /**
      * Returns the {@link TimerTask} which is associated with this handle.
      */
+    // 返回任务对象
     TimerTask task();
 
     /**
      * Returns {@code true} if and only if the {@link TimerTask} associated
      * with this handle has been expired.
      */
+    // 到达任务执行时间的状态，或者说是任务被执行过的状态，这个状态在任务被执行后更新
     boolean isExpired();
 
     /**
      * Returns {@code true} if and only if the {@link TimerTask} associated
      * with this handle has been cancelled.
      */
+    // 任务是否被取消
     boolean isCancelled();
 
     /**
@@ -50,5 +55,6 @@ public interface Timeout {
      *
      * @return True if the cancellation completed successfully, otherwise false
      */
+    // 取消执行任务
     boolean cancel();
 }
